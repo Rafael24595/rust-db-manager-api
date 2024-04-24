a72f3b4b1da60dcb5a3f79ce9bbcf5f6d179f0b3
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2024-04-24 22:57:13 +0200

    [FEATURE] CORS fixed:
    - DTOQueryPagination defaults included.

--- src/infrastructure/dto/dto_query_pagination.rs
+++ src/infrastructure/dto/dto_query_pagination.rs
@@ -2,6 +2,26 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct DTOQueryPagination {
+    #[serde(default = "default_offset")]
     pub offset: usize,
+    #[serde(default = "default_limit")]
     pub limit: usize,
+}
+
+fn default_offset() -> usize {
+    0
+}
+
+fn default_limit() -> usize {
+    10
+}
+
+impl DTOQueryPagination {
+    
+    pub fn new(offset: usize, limit: usize) -> DTOQueryPagination {
+        DTOQueryPagination {
+            offset, limit
+        }
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::net::SocketAddr;
 
+use tower_http::cors::CorsLayer;
+
 use axum::Router;
-use rust_db_manager_api::infrastructure::controller::Controller;
+use rust_db_manager_api::infrastructure::{controller::Controller, dto::dto_query_pagination::DTOQueryPagination, pagination::Pagination};
 use rust_db_manager_core::commons::configuration::configuration::Configuration;
 
 #[tokio::main]
@@ -9,7 +11,9 @@ async fn main() {
     let _ = Configuration::initialize();
 
     let router = Router::new();
-    let app = Controller::route(router).into_make_service_with_connect_info::<SocketAddr>();
+    let app = Controller::route(router)
+        .layer(CorsLayer::permissive())
+        .into_make_service_with_connect_info::<SocketAddr>();
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
