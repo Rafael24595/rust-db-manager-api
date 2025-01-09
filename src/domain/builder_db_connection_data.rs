ecf119edc1186842dc1626a51fd27b64cf74a6fa
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-01-10 00:42:22 +0100

    [FEATURE] Resolver list definition updated.

--- src/domain/builder_db_connection_data.rs
+++ src/domain/builder_db_connection_data.rs
@@ -1,4 +1,5 @@
 use rust_db_manager_core::{domain::connection_data::ConnectionData, infrastructure::repository::e_db_repository::EDBRepository};
+use url::Url;
 
 use crate::{commons::exception::api_exception::ApiException, infrastructure::dto::service::generate::dto_db_connection_data::DTODBConnectionData};
 
@@ -8,15 +9,28 @@ pub struct BuilderConnectionData {
 impl BuilderConnectionData {
     
     pub fn make(dto: DTODBConnectionData) -> Result<ConnectionData, ApiException> {
-        let category = EDBRepository::from_string(&dto.category);
-        if let None = category {
-            let message = format!("Data base type '{}' not supported.", dto.category);
-            return Err(ApiException::new(404, message));
-        }
-
+        let category = Self::identify(&dto);
         let data = ConnectionData::new(category.unwrap(), dto.connection);
-        
         Ok(data)
+    }
+
+    fn identify(dto: &DTODBConnectionData) -> Result<EDBRepository, ApiException> {
+        let url = Url::parse(&dto.connection);
+        if let Err(err) = url {
+            let exception = ApiException::new(404, err.to_string());
+            return Err(exception);
+        }
+        
+        let url = url.unwrap();
+    
+        match url.scheme().to_lowercase().as_str() {
+            "mongodb" => Ok(EDBRepository::MongoDB),
+            "postgres" => Ok(EDBRepository::Postgres),
+            _ => {
+                let exception = ApiException::new(404, format!("Invalid scheme '{}'.", url.scheme()));
+                Err(exception)
+            }
+        }
     }
 
 }
--- src/infrastructure/dto/service/definition/dto_service_category_lite.rs
+++ src/infrastructure/dto/service/definition/dto_service_category_lite.rs
@@ -1,4 +1,4 @@
-use rust_db_manager_core::infrastructure::repository::e_db_repository::EDBRepository;
+use rust_db_manager_core::infrastructure::repository::e_db_repository::{EDBRepository, EDBRepositoryItem};
 use serde::Serialize;
 
 use crate::infrastructure::db_assets::WebEDBRepository;
@@ -8,6 +8,7 @@ use super::dto_service_resources::DTOServiceResources;
 #[derive(Clone, Serialize)]
 pub struct DTOServiceCategoryLite {
     pub category: String,
+    pub default: bool,
     pub resources: DTOServiceResources
 }
 
@@ -16,7 +17,16 @@ impl DTOServiceCategoryLite {
     pub fn from(category: &EDBRepository) -> Self {
         Self {
             category: category.to_string(),
+            default: false,
             resources: category.resources()
+        }
+    }
+
+    pub fn from_item(category: &EDBRepositoryItem) -> Self {
+        Self {
+            category: category.item.to_string(),
+            default: category.default,
+            resources: category.item.resources()
         }
     }
 
