bdffb71039df543ebf36ecfc255a723fe9ee1648
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-02-07 01:39:32 +0100

    [FEATURE] Collection information updated.

--- src/infrastructure/controller_collection.rs
+++ src/infrastructure/controller_collection.rs
@@ -169,7 +169,7 @@ impl ControllerCollection {
         }
 
         let result = result.unwrap();
-        let locked_result = result.lock().await;
+        let mut locked_result = result.lock().await;
 
         let query = CollectionQuery::from(data_base, collection);
 
